9e67daed37f58c9dd42133fd023e6d0945bde202
nveronica <veronika_nagy@outlook.hu>
2020-08-19 22:21:33 +0200

    if there are no user in session->home link redirect to login.html

--- src/main/java/hu/citec/ediary/controller/HomeController.java
+++ src/main/java/hu/citec/ediary/controller/HomeController.java
@@ -3,22 +3,36 @@ package hu.citec.ediary.controller;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
-public class HomeController {
+public class HomeController{
+
     /*@GetMapping("/")
     String index(Principal principal) {
         return principal != null ? "home/homeSignedIn" : "home/homeNotSignedIn";
     }*/
     @GetMapping("/index")
-    public String home(Authentication authentication) {
-        boolean authenticated = authentication.isAuthenticated();
-        if (authenticated){
-            String loggedInAs = authentication.toString();//authentication.getAuthorities().toString();
+    public String home(Authentication auth, HttpSession session) {
+        boolean authenticated = false;
+        if (auth!=null) {
+           authenticated = auth.isAuthenticated();
+        }
+
+        if (session != null && authenticated){
+            String loggedInAs = auth.getName();//authentication.getAuthorities().toString();
             //authentication.getAuthorities().stream().findAny()
             return "redirect:/" + loggedInAs;
-        }else {
-        return "index";
         }
+
+        return "login";
+
     }
+    @RequestMapping("/index")
+    public String home(){
+        return "";
+    }
+
 }
