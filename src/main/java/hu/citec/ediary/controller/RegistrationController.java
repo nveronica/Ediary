94b8c4067f89792887ffcdfaf5ed99fdff76e9f8
Your Name <veronika_nagy@outlook.hu>
2021-12-24 18:36:33 +0100

    update

--- src/main/java/hu/citec/ediary/controller/RegistrationController.java
+++ src/main/java/hu/citec/ediary/controller/RegistrationController.java
@@ -3,8 +3,8 @@ package hu.citec.ediary.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.bind.annotation.RequestMapping;
+import hu.citec.ediary.model.CustomUser;
 import hu.citec.ediary.service.UserService;
 
 @Controller
@@ -12,18 +12,11 @@ public class RegistrationController {
 
 	@Autowired
 	UserService userService;
-	
-<<<<<<< HEAD
-    @GetMapping("/register")
-    public String registerNewUser(WebRequest request, Model model) {
-    	CustomU
-        model.addAttribute("user", userService);
-=======
+
     @RequestMapping("/register")
     public String registerNewUser(Model model) {
 	CustomUser newUser = new CustomUser();
         model.addAttribute("user", newUser);
->>>>>>> be3f7701092397d6108e451112b78692dddc28a9
         return "registration";
     }
 
